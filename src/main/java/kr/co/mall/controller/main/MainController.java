5ec8074bb99230fe220f521f1db88ff33d01beb5
main <daehan.ji@gmail.com>
2016-10-04 08:44:45 +0900

    no message

--- src/main/java/kr/co/mall/controller/main/MainController.java
+++ src/main/java/kr/co/mall/controller/main/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	@RequestMapping("/main.do")
-	public void main() {}
+	public void main() {
+		
+	}
 }
 
